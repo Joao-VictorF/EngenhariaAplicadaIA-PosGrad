a476e3d7a79095eeea25c7ba2e5623d06a1d1597
João Victor <freitasjoaovictor49@gmail.com>
2026-03-09 14:22:09 -0300

    feat: adding yolo model and prediction methods

--- exemplo-02/DuckHunt-JS-parte01/machine-learning/worker.js
+++ exemplo-02/DuckHunt-JS-parte01/machine-learning/worker.js
@@ -3,9 +3,11 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 const INPUT_MODEL_DIMENTIONS = 640
+const CLASS_THRESHOLD = 0.4
 
 let _labels = []
 let _model = null
+
 async function loadModelAndLabels() {
     await tf.ready()
 
@@ -66,6 +68,38 @@ async function runInference(tensor) {
         classes: classesData
     }
 }
+
+function* processPredictions({ boxes, scores, classes }, imageWidth, imageHeight) {
+    for (let i = 0; i < scores.length; i++) {
+        const score = scores[i]
+        if(score < CLASS_THRESHOLD) continue
+
+        const label = _labels[classes[i]]
+        if(label !== 'kite') continue
+
+        let [x1, y1, x2, y2] = boxes.slice(i * 4, (i + 1) * 4)
+
+        // Converter coordenadas normalizadas para pixels
+        x1 *= imageWidth
+        y1 *= imageHeight
+        x2 *= imageWidth
+        y2 *= imageHeight
+
+        // Calcular o centro da caixa delimitadora
+        const boxWidth = x2 - x1
+        const boxHeight = y2 - y1
+        
+        const centerX = x1 + boxWidth / 2
+        const centerY = y1 + boxHeight / 2
+        
+        yield {
+            x: centerX,
+            y: centerY,
+            score: (scores[i] * 100).toFixed(2),
+        }
+    }
+}
+
 loadModelAndLabels()
 
 self.onmessage = async ({ data }) => {
@@ -77,15 +111,14 @@ self.onmessage = async ({ data }) => {
 
     const inferenceResults = await runInference(input)
 
-    debugger
-    postMessage({
-        type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
-    });
-
-
+    processPredictions(inferenceResults, width, height)
+    for (const prediction of processPredictions(inferenceResults, width, height)) {
+        console.log(`🎯 Prediction: ${prediction.score}% confidence at (${prediction.x.toFixed(1)}, ${prediction.y.toFixed(1)})`);
+        postMessage({
+            type: 'prediction',
+            ...prediction            
+        });
+    }
 };
 
 console.log('🧠 YOLOv5n Web Worker initialized');
